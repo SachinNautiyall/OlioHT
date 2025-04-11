e9be1d02ddf02f35b79761410cc597f12eb1e6ac
SachinNautiyall <>
2025-04-11 17:55:45 +0300

    Fragment view code added

--- app/src/main/java/com/example/olioht/MainActivity.java
+++ app/src/main/java/com/example/olioht/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.olioht;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
+import com.example.olioht.fragments.HomeFragment;
+import com.example.olioht.fragments.MoveLutemonFragment;
 import com.google.android.material.tabs.TabItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +34,40 @@ public class MainActivity extends AppCompatActivity {
         TabItem BattleFragment = findViewById(R.id.BattleButton);
         TabItem AddFragment = findViewById(R.id.AddButton);
         TabItem StorageFragment = findViewById(R.id.StorageButton);
+        HomeFragment.setOnClickListener(listener);
+        MoveFragment.setOnClickListener(listener);
+        TrainFragment.setOnClickListener(listener);
+        BattleFragment.setOnClickListener(listener);
+        AddFragment.setOnClickListener(listener);
+        StorageFragment.setOnClickListener(listener);
+
 
     }
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Fragment fragment = null;
+
+            int id = view.getId();
+            if (id == R.id.HomeButton){
+                fragment = new HomeFragment();
+            } else if (id== R.id.StorageButton){
+                fragment = new MoveLutemonFragment();
+            } else if (id== R.id.BattleButton) {
+                    fragment = new MoveLutemonFragment();
+            } else if (id== R.id.TrainButton) {
+                fragment = new MoveLutemonFragment();
+            } else if (id== R.id.AddButton){
+                fragment = new MoveLutemonFragment();
+            }
+            if (fragment !=null){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.MainFragmentView, fragment)
+                        .commit();
+
+            }
+        }
+    };
 }
+
+
